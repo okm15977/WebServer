ee121d0cd9ad053230427917533e9fa692595964
okm15977 <31923818+okm15977@users.noreply.github.com>
2017-10-11 20:16:08 +0800

    Node.js建的 WebServer

---
+++ server.js
@@ -0,0 +1,23 @@
+//引用 node.js 內建的 http 模組
+const http = require('http');
+
+//建立一個 HttpServer
+//req 從 client端發出來的
+//res 是由我們寫出去給 client
+var server =  http.createServer(function(req,res){//createServer 只有建立 沒有執行
+    var resHeader = {
+        'Accept-Charset': 'utf-8',
+        'Accept-Language': 'zh-TW',
+        'Content-Type':'text/html;charset = utf-8',
+    };
+    res.writeHead(200,resHeader);
+    //writeHead是 寫 Headers
+    //write 是 寫 Response
+    res.write('<h1>Hello</h1>','utf8');
+    res.write('<p>這是由node.js建立的 WebServer</p>','utf8');
+    res.end();//沒寫end 可能資料只傳一半，end確保資料都送出去
+});
+
+//將 Server開啟 port 1234 執行起來
+server.listen(1234);//去網卡聽看看有沒有port= 1234的
+console.log("Server running at http://127.0.0.1:1234");
